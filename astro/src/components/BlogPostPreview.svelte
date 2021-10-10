5396518089a38ff7459fdbec39dbe01ad6275ade
Oli Nelson <oliver.nelson@nirovision.com>
2021-10-10 14:41:39 +1100

    wip

--- astro/src/components/BlogPostPreview.svelte
+++ astro/src/components/BlogPostPreview.svelte
@@ -1,22 +1,22 @@
 <script lang="ts">
-  import moment from "moment";
+  import moment from "moment"
 
-  import Card from "./Card.svelte";
-  export let post;
+  import Card from "./Card.svelte"
+  export let post
 
   interface Post {
-    Cover: string;
-    layout: string;
-    Name: string;
-    Description: string;
-    Published: string;
-    Author: string;
-    HeroImageDescription: string;
-    url: string;
+    Cover: string
+    layout: string
+    Name: string
+    Description: string
+    Published: string
+    Author: string
+    HeroImageDescription: string
+    url: string
   }
 
   const { Name, Description, Cover, HeroImageDescription, Published, url } =
-    post as Post;
+    post as Post
 </script>
 
 <Card onClick={() => (window.location = url)}>
@@ -28,8 +28,8 @@
     />
     <div class="p-2">
       <div class="text-xl font-semibold">{Name}</div>
-      <div class="text-xs">{moment(Published).calendar()}</div>
-      <div class="text-sm mt-2">
+      <div class="text-xs">{moment(Published).format("DD/MM/YY")}</div>
+      <div class="text-sm mt-2 overflow-ellipsis">
         {Description}
       </div>
     </div>
--- astro/src/components/VideoHero.svelte
+++ astro/src/components/VideoHero.svelte
@@ -1,22 +1,39 @@
 <script lang="ts">
-  export let src: string;
-  import { onMount } from "svelte";
+  import { fade } from "svelte/transition"
+  export let videoSrc: string = ""
+  export let imageSrc: string = ""
+  export let description: string = ""
 
-  let video;
+  let video: HTMLVideoElement | undefined = undefined
+  let showVideo = false
 
-  onMount(() => {
-    video.playbackRate = 0.3;
-  });
+  const onPlay = () => (showVideo = true)
+  const childClasses = "absolute top-0  w-full h-full object-cover"
 </script>
 
-<video bind:this={video} muted autoplay loop {src} class="grayscale" />
-
-<style>
-  video {
-    object-fit: cover;
-    height: 100vh;
-    width: 100vw;
-    /* position: sticky;
-    top: 0; */
-  }
-</style>
+<div
+  id="container"
+  class="bg-cover bg-center relative h-screen w-screen"
+  style="background-image: url('/assets/hendo_guitar_clip_poster.png');"
+>
+  {#if !showVideo}
+    <img
+      transition:fade={{ duration: 2000 }}
+      src={imageSrc}
+      alt={description}
+      class={`grayscale z-20 ${childClasses}`}
+    />
+  {/if}
+  <video
+    on:play={onPlay}
+    bind:this={video}
+    playsinline
+    loop
+    autoplay
+    muted
+    src={videoSrc}
+    poster={imageSrc}
+    alt={description}
+    class={`grayscale z-10 ${childClasses}`}
+  />
+</div>
