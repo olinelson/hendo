7dd6f255593a18c46f26d9b67e86fa474d3811e8
Oli Nelson <oliver.nelson@nirovision.com>
2022-01-02 12:57:38 +1100

    wip

--- src/components/BlogPostHeader.svelte
+++ src/components/BlogPostHeader.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
   import moment from "moment"
 
-  export let Name: string = ""
-  export let Description: string = ""
-  export let Published: string = ""
+  export let title: string = ""
+  export let description: string = ""
+  export let published: string = ""
 </script>
 
 <div class="flex justify-between items-end">
   <div>
-    <h1 class="font-bold text-4xl">{Name}</h1>
+    <h1 class="font-bold text-4xl">{title}</h1>
   </div>
-  <p class="font-light">{moment(Published).format("Do MMM YYYY")}</p>
+  <p class="font-light">{moment(published).format("Do MMM YYYY")}</p>
 </div>
